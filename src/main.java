43141143fbe01fd351d549cfb387d3cf3b86963c
Nikita Smirnov <n@newair.ru>
2017-10-21 14:00:04 +0300

    random

--- src/main.java
+++ src/main.java
@@ -1,19 +1,57 @@
-import java.util.Scanner;
+import javax.swing.*;
+import java.awt.*;
 
-public class main {
-    public static void main(String[] args) {
-        long i;
-        Scanner sc = new Scanner(System.in);
-        i = sc.nextInt();
-        long y = i / 2;
-        if (i % 2 == 0) {
-            System.out.println((i + 1) * i / 2);
-        } else {
-            System.out.println((i + 1) * i / 2);
+class Panel extends JPanel {
+    int house_x = 150, house_y = 150;
+
+    Panel() {
+        Timer nt = new Timer(15, e -> repaint());
+        nt.start();
+    }
+
+    private void drawHouse(Graphics g, int x, int y, Color color) {
+        g.setColor(color);
+        g.fillRect(x - 50, y - 50, 100, 100);
+
+        Polygon polygon = new Polygon();
+        polygon.addPoint(x - 70, y - 50);
+        polygon.addPoint(x + 70, y - 50);
+        polygon.addPoint(x, y - 90);
+        g.fillPolygon(polygon);
+
+
+
+    }
+
+    public void paintComponent(Graphics g) {
+        Rectangle canvas = g.getClipBounds();
+        g.clearRect(0, 0, canvas.width, canvas.height);
+        drawHouse(g, house_x, house_y, Color.RED);
+
+
+        if (house_x<601) {
+            house_x +=5;
         }
 
+        if (house_x == 600) {
+            house_x -=8;
+        }
     }
 }
 
+class Frame extends JFrame {
+    Frame(int width, int height) {
+        Panel panel = new Panel();
+        Container container = getContentPane();
+        container.add(panel);
+        setSize(width, height);
+        setVisible(true);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
 
-1
+public class main {
+    public static void main(String[] args) {
+        Frame window = new Frame(800, 600);
+    }
+}
