7904775fe25ab3025216f0640ef54e4281ca67e7
Nikita Smirnov <n@newair.ru>
2017-10-21 12:12:45 +0300

    Initial commit

---
+++ src/main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class main {
+    public static void main(String[] args) {
+        long i;
+        Scanner sc = new Scanner(System.in);
+        i = sc.nextInt();
+        long y = i / 2;
+        if (i % 2 == 0) {
+            System.out.println((i + 1) * i / 2);
+        } else {
+            System.out.println((i + 1) * i / 2);
+        }
+
+    }
+}
+
+
+
+
