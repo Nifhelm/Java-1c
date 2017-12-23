dfec09707fc27898fe19408291362509e0c998e6
Nikita Smirnov <n@newair.ru>
2017-12-23 12:44:47 +0300

    Update main.java

--- src/main.java
+++ src/main.java
@@ -1,65 +1,179 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.io.IOException;
+
+
+class  Krug {
+     int x, y;
+
+
+
+     Image sprite;
+
+     public Krug(int x, int y) throws IOException {
+         sprite = ImageIO.read(new java.io.File("C:\\Users\\user\\Desktop\\08bf4fc519daedce69e0cda9a7b1414a.jpg"));
+         this.x = x;
+         this.y = y;
+     }
+
+
+     void move(Rectangle boundaries) {
+         if (x < boundaries.width && y <  boundaries.height ) {
+             x += 5;
+
+         } else {
+             x -= 5;
+             y += 5;
+         }
+
+         if (x > boundaries.width && y > boundaries.height) {
+             x -= 5;
+         }
+     }
+
+     void draw(Graphics g) {
+         g.drawImage(sprite, x ,y , 100, 100, null);
+     }
+ }
+
+class Boat {
+    int x, y;
+
+    Image sprite;
+
+    public Boat(int x, int y) throws IOException {
+        this.x = x;
+        this.y = y;
+        sprite = ImageIO.read(new java.io.File("C:\\Users\\user\\Desktop\\img5.jpg"));
+    }
+
+    void processKey(int currentKeyCode) {
+
+        if (currentKeyCode == KeyEvent.VK_UP) {
+            y -=3;
+        } else if (currentKeyCode == KeyEvent.VK_DOWN) {
+            y +=3;
+        } else  {
+
+        }
+    }
+
+    void draw(Graphics g) {
+        g.drawImage(sprite, x ,y , 4, 100, null);
+    }
+}
+
+class Boat2 {
+    int x, y;
+
+    Image sprite;
+
+    public Boat2(int x, int y) throws IOException {
+        this.x = x;
+        this.y = y;
+        sprite = ImageIO.read(new java.io.File("C:\\Users\\user\\Desktop\\img5.jpg"));
+    }
+
+
+    void processKey(int currentKeyCode) {
+
+        if (currentKeyCode == KeyEvent.VK_NUMPAD8) {
+            y -=3;
+        } else if (currentKeyCode == KeyEvent.VK_NUMPAD5) {
+            y +=3;
+        }
+
+
+    }
+
+    void draw(Graphics g) {
+        g.drawImage(sprite, x ,y , 4, 100, null);
+
+    }
+}
 
 class Panel extends JPanel {
-    int house_x = 150, house_y = 150;
+    Boat boat;
+    Boat2 boat2;
+    Krug krug;
 
-    Panel() {
-        Timer nt = new Timer(15, e -> repaint());
+
+
+    Panel() throws IOException {
+        Timer nt = new Timer(10, e -> repaint());
         nt.start();
+
+        addKeyListener(new KeyListener() {
+            @Override
+            public void keyTyped(KeyEvent e) {
+            }
+
+            @Override
+            public void keyPressed(KeyEvent e) {
+                int currentKeyCode = e.getKeyCode();
+                if (currentKeyCode == KeyEvent.VK_NUMPAD8) {
+                    boat2.y -=3;
+                } else if (currentKeyCode == KeyEvent.VK_NUMPAD5) {
+                    boat2.y +=3;
+                } else  {
+
+                }
+
+
+                if (currentKeyCode == KeyEvent.VK_UP) {
+                    boat.y -=3;
+                } else if (currentKeyCode == KeyEvent.VK_DOWN) {
+                    boat.y +=3;
+                } else  {
+
+                }
+            }
+
+
+           @Override
+            public void keyReleased(KeyEvent e) {
+//                currentKeyCode = 0;
+            }
+        });
+
+        setFocusable(true);
+        boat = new Boat(2, 2);
+        boat2 = new Boat2(770, 0);
+        krug = new Krug(80, 80);
+
+
     }
 
-    private void drawHouse(Graphics g, int x, int y, Color color) {
-        g.setColor(color);
-        g.fillRect(x - 50, y - 50, 100, 100);
 
-        Polygon polygon = new Polygon();
-        polygon.addPoint(x - 70, y - 50);
-        polygon.addPoint(x + 70, y - 50);
-        polygon.addPoint(x, y - 90);
-        g.fillPolygon(polygon);
-
-
-
-    }
 
     public void paintComponent(Graphics g) {
         Rectangle canvas = g.getClipBounds();
         g.clearRect(0, 0, canvas.width, canvas.height);
-        drawHouse(g, house_x, house_y, Color.RED);
+        krug.move(canvas);
 
-
-        if (house_x < 750 && house_y == 150) {
-                house_x +=5;
-
-        }
-
-        else  {
-                house_x -=5;
-                house_y +=5;
-        }
-
-
-        if (house_x > 500 && house_y > 200) {
-            house_x -=5;
-            house_y -=5;
-        }
+        boat.draw(g);
+        boat2.draw(g);
+        krug.draw(g);
     }
 }
 
 class Frame extends JFrame {
-    Frame(int width, int height) {
+    Frame(int width, int height) throws IOException {
         Panel panel = new Panel();
         Container container = getContentPane();
         container.add(panel);
         setSize(width, height);
         setVisible(true);
-        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
     }
 }
 
-public class main {
-    public static void main(String[] args) {
-        Frame window = new Frame(800, 600);
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        new Frame(800, 600);
     }
 }
